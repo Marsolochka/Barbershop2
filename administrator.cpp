92d8087352c855ab6cefb6d635d5d423302b1e12
ElinaTanicheva <elina.tanicheva@icloud.com>
2023-12-25 17:55:36 +0300

    Добавление метода для загрузки информации об объектах из файла и заполнения items на основе данных из этого файла.

--- administrator.cpp
+++ administrator.cpp
@@ -236,4 +236,21 @@ void Administrator::saveItemsToFile(const std::string& filename) {
            throw std::runtime_error("Unable to open file for writing: " + filename);
        }
    }
-
+void Administrator::loadItemsFromFile(const std::string& filename) {
+       std::ifstream file(filename);
+       if (file.is_open()) {
+           items.clear(); // Очищаем текущие элементы
+           std::string line;
+           while (std::getline(file, line)) {
+               size_t pos = line.find(",");
+               if (pos != std::string::npos) {
+                   std::string name = line.substr(0, pos);
+                   std::string description = line.substr(pos + 1);
+                   items.emplace_back(name, description);
+               }
+           }
+           file.close();
+       } else {
+           throw std::runtime_error("Unable to open file for reading: " + filename);
+       }
+   }
